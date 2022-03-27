17085f21e1f482276e129eb047ed084970b8e1ec
amreshkumar <amresh.kumar2@olx.com>
2022-03-28 00:21:38 +0530

    Very First Commit

---
+++ src/test/java/com/amresh/demogit/DemogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amresh.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
